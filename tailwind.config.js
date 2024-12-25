8b08251e1c84ef728d52066ba1174adac4ea989f
VijayDeshwal339 <deshwalamit339@gmail.com>
2024-12-25 12:24:51 +0530

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,35 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'DIN': ['DINPro'],
+        'Nun': ["Nunito"],
+        'Poppin':["Poppins"],
+        'Inter':["Inter"],
+      },
+      colors: {
+        customGreen: '#F4F7EC',
+        customTransparent: 'rgba(248, 248, 247, 0.46)',
+      },
+      screens: {
+        'xs': '0px',
+        'sm': '480px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+  
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+      }
+    },
   },
   plugins: [],
 };
